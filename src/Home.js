f04388c59dbce35fb4e4c4bc06ff852a17cbe440
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2025-07-10 01:56:23 +0300

    Update Home.js
    Test

--- src/Home.js
+++ src/Home.js
@@ -74,7 +74,7 @@ function GallerySection({ id, title, images = [], videos = [] }) {
         {images.map((img, index) => (
           <img
             key={index}
-            src={`${process.env.PUBLIC_URL}/images/${img}`}
+            src={`${process.env.PUBLIC_URL}/${img}`}
             alt={`${title} ${index + 1}`}
             loading="lazy"
           />
