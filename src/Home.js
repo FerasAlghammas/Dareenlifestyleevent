6678584f37dce7e22dfe2c151f1a9028297fedd0
Moaid Ali Alshamrani <moaid.alshamrani@gmail.com>
2025-08-06 07:14:15 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -66,7 +66,7 @@ function GallerySection({ id, title, images = [], videos = [] }) {
         {videos.map((vid, index) => (
           <video key={index} controls preload="metadata">
             <source src={`${process.env.PUBLIC_URL}/videos/${vid}`} type="video/mp4" />
-            Your browser does not support the video tag.
+            
           </video>
         ))}
 
